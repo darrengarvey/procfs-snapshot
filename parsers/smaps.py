ea71612ebf8a9e53b19628c5aef1dd6a9d6e2c16
Darren Garvey <darren.garvey@gmail.com>
2016-08-16 23:14:26 +0100

    Parse the rest of the smaps header.

--- parsers/smaps.py
+++ parsers/smaps.py
@@ -4,7 +4,9 @@ from model import SmapsPermissions, MemoryRegion
 def parse_smaps_header(header):
     info = MemoryRegion()
     # Example line is:
-    # 011e6000-01239000 rw-p 00000000 00:00 0  [heap]
+    # 011e6000-01239000 rw-p 00000000 00:00 0    [heap]
+    # 8ec00000-8ec01000 rw-s 00000000 00:14 20   /dev/shm/NS2371 (deleted)
+    # All numbers are hex except for the inode
     parts = header.split()
     print (parts)
 
@@ -19,6 +21,18 @@ def parse_smaps_header(header):
     info.permissions.private = "p" in permissions
     info.permissions.shared = "s" in permissions
 
-    info.name = parts[-1]
+    info.offset = int(parts[2], 16)
+
+    # eg. 08:06
+    info.major_dev, info.minor_dev = [int(x, 16) for x in parts[3].split(':')]
+
+    # The inode isn't a hex number
+    info.inode = int(parts[4])
+
+    # eg. [heap]
+    # or  /dev/shm/NS2371
+    info.name = parts[5]
+
+    info.deleted = header.endswith('(deleted)')
 
     return info
--- test/test_smaps_parsing.py
+++ test/test_smaps_parsing.py
@@ -11,19 +11,21 @@ class SmapsParserTest(unittest.TestCase):
         with open(os.path.join(os.path.dirname(__file__), 'firefox.smaps'), 'rb') as f:
             self.data = f.read()
         # Another bunch of example lines for various cases.
-        self.process_line = '55917d4f7000-55917d514000 r-xp 00000000 08:06 542715                     /usr/lib/firefox/firefox'
+        # These are faked a bit to make the tests more useful
+        # (eg. offsets are faked).
+        self.process_line = '55917d4f7000-55917d514000 r-xp 00100000 08:06 542715                     /usr/lib/firefox/firefox'
         self.lib_line = '7f5c3f7fd000-7f5c3f7fe000 rw-p 00049000 08:06 532533                     /usr/lib/x86_64-linux-gnu/libopus.so.0.5.2'
-        self.heap_line = '011e6000-01239000 rw-p 00000000 00:00 0                                  [heap]'
-        self.stack_line = '7ffefddd2000-7ffefde0d000 rw-p 00000000 00:00 0                          [stack]'
-        self.vdso_line = '7ffefdebc000-7ffefdebe000 r-xp 00000000 00:00 0                          [vdso]'
-        self.shared_mem_line = '7f5cabdce000-7f5cabe4e000 rw-s 00000000 00:05 1605639                    /SYSV00000000 (deleted)'
-        self.anonymous_line = '7f5cabd5d000-7f5cabd61000 rw-p 00000000 00:00 0 '
+        self.heap_line = '011e6000-01239000 rw-p 00003000 00:00 0                                  [heap]'
+        self.stack_line = '7ffefddd2000-7ffefde0d000 rw-p 00001f00 00:00 0                          [stack]'
+        self.vdso_line = '7ffefdebc000-7ffefdebe000 r-xp 00000020 00:00 0                          [vdso]'
+        self.shared_mem_line = '7f5cabdce000-7f5cabe4e000 rw-s 0060e000 00:05 1605639                    /SYSV00000000 (deleted)'
+        self.anonymous_line = '7f5cabd5d000-7f5cabd61000 rw-p 00007000 00:00 0 '
 
     def test_heap_parsing(self):
         info = parse_smaps_header(self.heap_line)
-        self.assertEqual(int('011e6000', 16), info.start_addr)
-        self.assertEqual(int('01239000', 16), info.end_addr)
-        self.assertEqual(int('01239000', 16) - int('011e6000', 16), info.size)
+        self.assertEqual(0x011e6000, info.start_addr)
+        self.assertEqual(0x01239000, info.end_addr)
+        self.assertEqual(0x01239000 - 0x011e6000, info.size)
         # Check perms
         self.assertTrue(info.permissions.readable)
         self.assertTrue(info.permissions.writable)
@@ -31,9 +33,40 @@ class SmapsParserTest(unittest.TestCase):
         self.assertTrue(info.permissions.private)
         self.assertFalse(info.permissions.shared)
 
+        self.assertEqual(0x00003000, info.offset)
+
+        self.assertEqual(0, info.major_dev)
+        self.assertEqual(0, info.minor_dev)
+
+        self.assertEqual(0, info.inode)
+
         self.assertEqual('[heap]', info.name)
 
+        self.assertFalse(info.deleted)
 
+
+    def test_shared_mem_parsing(self):
+        info = parse_smaps_header(self.shared_mem_line)
+        self.assertEqual(0x7f5cabdce000, info.start_addr)
+        self.assertEqual(0x7f5cabe4e000, info.end_addr)
+        self.assertEqual(0x7f5cabe4e000 - 0x7f5cabdce000, info.size)
+        # Check perms
+        self.assertTrue(info.permissions.readable)
+        self.assertTrue(info.permissions.writable)
+        self.assertFalse(info.permissions.executable)
+        self.assertFalse(info.permissions.private)
+        self.assertTrue(info.permissions.shared)
+
+        self.assertEqual(0x0060e000, info.offset)
+
+        self.assertEqual(0, info.major_dev)
+        self.assertEqual(5, info.minor_dev)
+
+        self.assertEqual(1605639, info.inode)
+
+        self.assertEqual('/SYSV00000000', info.name)
+
+        self.assertTrue(info.deleted)
 
 
 if __name__ == '__main__':

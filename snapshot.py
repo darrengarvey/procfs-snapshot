16aa5c5353a694d8b9febe1e1075f8f767b83fcf
Darren Garvey <darren.garvey@gmail.com>
2016-08-17 13:03:03 +0100

    Allow filtering on one or more pids.
    >1 pid is only supported through bash expansion magic, not very obvious.

--- snapshot.py
+++ snapshot.py
@@ -7,6 +7,9 @@ def parse_args():
     parser = ArgumentParser(description='Snapshot statistics from a machine')
     parser.add_argument('--ip', default='',
                         help='connect to a remote host (recommended)')
+    # Multiple pids could be set using bash expansion: {1234,2345}
+    parser.add_argument('-p', '--pid', default='*',
+                        help='the pid(s) to look up (default: *)')
     parser.add_argument('-u', '--user', default='root',
                         help='user to log into remote host with (default: root)')
     parser.add_argument('-v', '--verbose', action='store_true',
@@ -23,19 +26,19 @@ def main(args):
         LOGGER.setLevel(logging.INFO)
     if args.ip == '':
         print ('Loading local procfs files')
-        cmd = 'sudo bash -c "tail -n +1 /proc/*/{cmdline,smaps}"'
+        cmd = 'sudo bash -c "tail -n +1 /proc/%s/{cmdline,smaps}"' % args.pid
         stream = Popen(cmd, shell=True, bufsize=-1, stdout=PIPE).stdout
     elif args.ip != '':
-        cmd = """ssh %s@%s 'bash -c "tail -n +1 /proc/*/{cmdline,smaps} 2>/dev/null"'""" % \
-              (args.user, args.ip)
+        cmd = """ssh %s@%s 'bash -c "tail -n +1 /proc/{}/{cmdline,smaps} 2>/dev/null"'""".format(
+              (args.user, args.ip, args.pid))
         #tail /proc/*/{cmdline,smaps}\'' % args.hostname
         stream = Popen(cmd, shell=True, bufsize=-1, stdout=PIPE).stdout
 
     LOGGER.info('Reading procfs with cmd: %s' % cmd)
     processes, memory_regions = read_tailed_files(stream)
 
-    LOGGER.info('Found %d processes and %d used memory fragments'.format(
-                (len(processes), len(memory_regions))))
+    LOGGER.info('Found {} process(es) and {} used memory fragments'.format(
+                len(processes), len(memory_regions)))
     LOGGER.info('Regions: %s' % memory_regions)
 
 if __name__ == '__main__':

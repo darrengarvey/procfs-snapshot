d96e504bc6112a09d10ee2b201bfedc9c6c7429c
Darren Garvey <darren.garvey@gmail.com>
2016-08-18 20:26:10 +0100

    Fix up tests

--- test/test_tail_parsing.py
+++ test/test_tail_parsing.py
@@ -8,18 +8,18 @@ from parsers.tail import read_tailed_files
 class TailParserTest(unittest.TestCase):
     
     def test_reading_empty_string(self):
-        processes = read_tailed_files(StringIO(''))
+        processes, _ = read_tailed_files(StringIO(''))
         self.assertEqual(0, len(processes))
 
     def test_reading_empty_file(self):
         with tempfile.TemporaryFile('rb') as f:
-            processes = read_tailed_files(f)
+            processes, _ = read_tailed_files(f)
             self.assertEqual(0, len(processes))
 
 
     def test_reading_realistic_file(self):
         with open(os.path.join(os.path.dirname(__file__), 'procfs.tail'), 'rb') as f:
-            processes = read_tailed_files(f)
+            processes, _ = read_tailed_files(f)
             self.assertEqual(6, len(processes))
 
 if __name__ == '__main__':

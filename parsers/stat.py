fcbe3b3d60e7b63204d1cabc941cf8223eb1703b
Lukasz Forynski <lukasz.forynski@gmail.com>
2016-10-07 22:08:10 +0100

    Refactor parsers: update stat to use new style parsing for internal implementation
    This is a next part of a re-factoring attempt to make parsers generic.
    
    This no functional change updates the existing implementation for stat
    to use new style parsing.

--- parsers/stat.py
+++ parsers/stat.py
@@ -108,27 +108,24 @@ def parse_stat(obj, data):
     if not (isinstance(obj, Process) or isinstance(obj, Thread)):
         raise TypeError('%s is not of type Process or Thread' % type(obj))
 
-    parts = data.split()
-    # Parse data from /proc/*/stat or /proc/*/task/*/stat.
-    # We only collect a subset of this to reduce the amount of data that
-    # needs to be collected. More can be collected if it's useful.
-    # eg. (sqlitebrowser) 46582 70 6605 429 8244323
+    parser = Parser_stat()
+    res = parser.parse(data, dict())['proc_stat']
 
     # comm %s
     # (2) The filename of the executable, in parentheses. This is visible
     # whether or not the executable is swapped out.
     # Note that this is also the thread name, if set.
-    obj.comm = parts[1][1:-1]  # omit the brackets
+    obj.comm = res['comm']
 
     # minflt %lu
     # (10) The number of minor faults the process has made which have not
     # required loading a memory page from disk.
-    obj.minor_faults = int(parts[9])
+    obj.minor_faults = res['minflt']
 
     # majflt %lu
     # (12) The number of major faults the process has made which have
     # required loading a memory page from disk.
-    obj.major_faults = int(parts[11])
+    obj.major_faults = res['majflt']
 
     # utime %lu
     # (14) Amount of time that this process has been scheduled in user
@@ -136,16 +133,16 @@ def parse_stat(obj, data):
     # This includes guest time, guest_time (time spent running a virtual
     # CPU, see below), so that applications that are not aware of the
     # guest time field do not lose that time from their calculations.
-    obj.user_time = int(parts[13])
+    obj.user_time = res['utime']
 
     # stime %lu
     # (15) Amount of time that this process has been scheduled in kernel
     # mode, measured in clock ticks (divide by sysconf(_SC_CLK_TCK)).
-    obj.system_time = int(parts[14])
+    obj.system_time = res['stime']
 
     # starttime %llu (was %lu before Linux 2.6) 
     # (22) The time the process started after system boot. In kernels
     # before Linux 2.6, this value was expressed in jiffies. Since Linux
     # 2.6, the value is expressed in clock ticks (divide by
     #   ).
-    obj.start_time = int(parts[21])
+    obj.start_time = res['starttime']
